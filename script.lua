94ac74752aa6ff46b09325fa28df8a31ffeeb5a8
MistrAnonym <anonymmistr@gmail.com>
2024-07-28 20:30:30 +0200

    Delete script.lua
    Not working script

--- script.lua
+++
@@ -1,6 +0,0 @@
-local response = fetch({
-    url = "https://api.buss.lol/domains",
-    method = "GET", -- HTTP REQUEST METHOD
-    headers = { ["Content-Type"] = "application/json" },
-    body = '{ "test": ' .. test .. '}' -- REQUEST BODY
-}}
